c275d788be486c6320d121c11591d3c739ef4c20
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-29 04:22:30 +0530

    Add project files.

---
+++ Type 4- Named parameter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Type_4__Named_parameter
+{
+    class Program
+    {
+        public void show(int age, string name, double marks)
+        {
+            Console.WriteLine("My "+age+ " "+name+ " "+marks);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.show(name: "akshay saini", marks: 500, age: 21);
+            obj.show(marks: 500, name: "akshay saini",  age: 21);
+        }
+    }
+}
